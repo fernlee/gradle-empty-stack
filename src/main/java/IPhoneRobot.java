078eddcb191a224a022b1151d58c3198199a8f8d
yaeli <yaeli@thoughtworks.com>
2019-07-01 00:33:22 +0800

    [yae][implement story3 - IPhoneRobot can only use iPhone]

---
+++ src/main/java/IPhoneRobot.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yeli on 2019/7/1.
+ */
+public class IPhoneRobot extends Person {
+    public IPhoneRobot(String name, IPhone iPhone) {
+        super(name, iPhone);
+    }
+}
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,7 +5,7 @@ public class Person {
     private String name;
     private Mobile mobile;
 
-    public Person(String name, Mobile mobile, int messageLengthLimit) {
+    public Person(String name, Mobile mobile) {
         this.name = name;
         this.mobile = mobile;
     }
